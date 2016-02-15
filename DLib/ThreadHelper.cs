c5fe344c3756b95e9af450c51403a21a3459cb04
db <rocky_ting@qq.com>
2016-02-15 18:06:46 +0800

    ThreadHelper

---
+++ DLib/ThreadHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLib
+{
+    public static class ThreadHelper
+    {
+        public static void SafeExecute(this Action test, out Exception exception)
+        {
+            exception = null;
+
+            try
+            {
+                test.Invoke();
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+        }
+    }
+}
