3eb502d9683beb360ae2d88cda5e69ad49efe2a3
db <rocky_ting@qq.com>
2016-03-01 14:16:19 +0800

    EnumUtil

---
+++ DLib/EnumUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLib
+{
+    public static class EnumUtil
+    {
+        //StatusEnum MyStatus = EnumUtil.ParseEnum<StatusEnum>("Active");
+        public static T ParseEnum<T>(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        //StatusEnum MyStatus = "Active".ToEnum<StatusEnum>();
+        public static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        //StatusEnum MyStatus = "Active".ToEnum(StatusEnum.None);
+        public static T ToEnum<T>(this string value, T defaultValue) where T : struct
+        {
+            if (value == null || value.Trim() == "")
+            {
+                return defaultValue;
+            }
+
+            T result;
+            return Enum.TryParse<T>(value, true, out result) ? result : defaultValue;
+        }
+    }
+}
