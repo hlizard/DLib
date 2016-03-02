a6160520f0342f6401bcbecc62adb46c89649919
db <rocky_ting@qq.com>
2016-03-02 10:47:33 +0800

    StringEx

---
+++ DLib/StringEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLib
+{
+    public static class StringEx
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return str == null || str.Trim() == string.Empty;
+        }
+    }
+}
