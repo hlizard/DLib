cde6a3c204a5874c079eca36a1e1af1c748904ac
db <rocky_ting@qq.com>
2016-03-03 17:04:19 +0800

    可序列化的RangeValidator

---
+++ DLib/MyRangeValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web.UI.WebControls;
+
+namespace DLib
+{
+    [Serializable]
+    public class MyRangeValidator : RangeValidator, ISerializable
+    {
+        public MyRangeValidator() : base()
+        {
+            
+        }
+
+        protected MyRangeValidator(SerializationInfo info, StreamingContext context)
+        {
+            MaximumValue = info.GetString("MaximumValue");
+            MinimumValue = info.GetString("MinimumValue");
+            ErrorMessage = info.GetString("ErrorMessage");
+            //Type = (ValidationDataType)info.GetValue("Type", typeof(ValidationDataType));
+            Type = (ValidationDataType)info.GetInt32("Type");
+            //IsValid = info.GetBoolean("IsValid");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("MaximumValue", MaximumValue);
+            info.AddValue("MinimumValue", MinimumValue);
+            info.AddValue("ErrorMessage", ErrorMessage);
+            info.AddValue("Type", (int)Type);
+            //info.AddValue("IsValid", IsValid);  //for use DynamicJson dynamic //值不会由Validate更新
+        }
+    }
+}
